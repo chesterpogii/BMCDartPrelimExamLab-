dc90719d10fb9e4d0cec9f295b5ec8c23bab7427
Don Chester Jimenez <donchesterjimenez4@gmail.com>
2025-08-28 09:46:25 +0000

    Student Grade Evaluator  - Jimenez

---
+++  main.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+void main () { 
+    try{ //Display name stdout.write("Enter your name: ");
+    String? = stdn.readLineSync();
+    print("");
+    // Prompt for assignment score stdout.write ("Enter assignment score: ");
+    double assignmentScore = double parse stdn.readLineSync(); 
+    print("");
+    // Prompt for Midterm Exam stdout.write ("Enter midterm score: ");
+    double Midterm Exam = double parse stdn.readLineSync(); 
+    print("");
+    // Prompt for Final Exam stdout.write ("Enter final score: ");
+    double Final Exam = double parse stdn.readLineSync(); 
+    print("");
+
+    print('/All inputs are Valid'!);
+    print Assignment: $assignmentScore, Midterm: $midtermScore, Final: $finalExamScore'):
+    print("");
+    
+    double total = (assignmentScore+midtermScore)/3; 
+    print ("Your Total Grade is: $total"); }
+
+    catch (e) {
+        print('\invalid input. Please enter numeric values only.');
+        exit(1);
+        //Exit the program with error code
+         }
+    
+    }
+
+
+
+// Output 
+print ("Name: $name");
+print ("Final Grade: ${grade.toStringAsFixed(2)}");
+
+if (grade >= Passed) {
+    print("Passed");
+} else {
+    print("Failed");
+} 
