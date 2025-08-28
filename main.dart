4c99bd1140bc0f50f943733c4ea40811f0b864ed
Don Chester Jimenez <donchesterjimenez4@gmail.com>
2025-08-28 17:55:47 +0800

    main.dart

--- main.dart
+++ main.dart
@@ -4,31 +4,33 @@ void main () {
     String? = stdn.readLineSync();
     print("");
     // Prompt for assignment score stdout.write ("Enter assignment score: ");
+             // humihingi ng assignment exam score mula sa user
     double assignmentScore = double parse stdn.readLineSync(); 
     print("");
     // Prompt for Midterm Exam stdout.write ("Enter midterm score: ");
+             // humihingi ng midterm exam score mula sa user
     double Midterm Exam = double parse stdn.readLineSync(); 
     print("");
     // Prompt for Final Exam stdout.write ("Enter final score: ");
+        // humihingi ng final exam score mula sa user
     double Final Exam = double parse stdn.readLineSync(); 
     print("");
 
     print('/All inputs are Valid'!);
     print Assignment: $assignmentScore, Midterm: $midtermScore, Final: $finalExamScore'):
+    // pwede kang magdagdag ng code para icalculate and final grade at magprint ng result etc
     print("");
     
     double total = (assignmentScore+midtermScore)/3; 
-    print ("Your Total Grade is: $total"); }
-
+    print ("Your Total Grade is: $total"); }t 
     catch (e) {
+     // ihahandle nito and error kung sakaling may error or maling inpu
         print('\invalid input. Please enter numeric values only.');
         exit(1);
         //Exit the program with error code
          }
     
     }
-
-
 
 // Output 
 print ("Name: $name");
